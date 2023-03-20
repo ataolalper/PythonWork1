2f4b3da6b53db29bb150b2188b75d8a1cb723b83
Ataol Alper <74360152+ataolalper@users.noreply.github.com>
2023-03-20 12:05:14 +0300

    Add files via upload

---
+++ odev_1.py
@@ -0,0 +1,40 @@
+# Ödev - 1
+
+# Ataol ALPER
+
+# Python'da kullanılan veri tipleri
+
+# Giriş dersi içerisinde kullanılan veri tipleri:
+
+# String: Metinsel ifadelerin çıktılarının olduğu veri tipidir.
+# Numeric: Sayı veya rakam çıktılarının olduğu veri tipidir.
+    # Intager: Tam sayı veri tipidir.
+    # Float: Ondalık sayı veri tipidir.
+# Boolean: True veya False çıktıları veren mantıksal veri tipidir.
+
+# İnternet üzerinde araştırma sonucu bulduğum veri tipleri
+
+# Sequence: Sıralama veya liste yapma veri tipidir.
+# Mapping: Haritalama veri tipidir.
+# Set: Birden fazla türün bulunduğu küme olan veri tipidir.
+
+# kodlama.io sitesi üzerinde bulunan bazı değişken tipleri
+
+yazi1 = "(2023) Yazılım Geliştirici Yetiştirme Kampı - Python & Selenium" # String veri tipidir.
+
+tamamlananYuzde = 14 # Numeric veri tipidir
+
+ePosta = "adsoyad@gmail.com"
+sifre = "12345asdfg"
+
+girilenEPosta = "adsoyad@gmail.com"
+girilenSifre = "9876asdf"
+
+if ePosta == girilenEPosta:
+    if sifre == girilenSifre:
+        print("Giriş başarılı")
+    else:
+        print("E-posta veya şifreniz yanlıştır.")
+else:
+    print("Girilen e-posta yanlıştır.")
+
